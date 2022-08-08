ca7490d06dbb6aa369250dfa4604192cad9238c1
lihaosongbluejeans <44400254+lihaosongbluejeans@users.noreply.github.com>
2022-08-09 00:05:58 +0800

    第二个项目 加

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello/_3.dart';
 
 main() {
-  runApp(const MyApp());
+  runApp(MyApp())
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'hello Flutter',
-        theme: ThemeData(primarySwatch: Colors.yellow),
-        home: Scaffold(
-            appBar: AppBar(title: const Text("HELLO FLUTTER")),
-            body: Center(
-                child: Text(
-              "HELLO FLUTTER",
-              style: Theme.of(context).textTheme.headline4,
-            ))));
-  }
-}
+class MyApp()
